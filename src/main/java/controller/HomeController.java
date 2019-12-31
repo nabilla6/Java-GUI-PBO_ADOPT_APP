10762c1b47df1a6a0fc122a21eb2a0e74b8b1056
Nabil la <nabilla060@gmail.com>
2019-12-31 23:26:37 +0700

    First

---
+++ src/main/java/controller/HomeController.java
@@ -0,0 +1,30 @@
+package controller;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class HomeController {
+
+    @FXML
+    private AnchorPane content;
+
+    @FXML
+    private JFXButton adoptBtn;
+
+    @FXML
+    private JFXButton laterBtn;
+
+    @FXML
+    void handleAdopt(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleLater(ActionEvent event) {
+
+    }
+}
